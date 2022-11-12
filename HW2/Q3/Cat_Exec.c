018e7b860ed2ec84453984540b341d216bc07c9d
Navid <navid8020@gmail.com>
2022-11-12 10:53:23 +0330

    Cat with Exec

--- HW2/Q3/Cat_Exec.c
+++ HW2/Q3/Cat_Exec.c
@@ -12,15 +12,23 @@ void catFunction(char* arg)
     my_args[2] = NULL;
     pid_t pid = fork();
     if (pid == 0)
+    {
         execvp(my_args[0], my_args);
+    }
     else if(pid > 0)
+    {
         waitpid(pid, NULL, 0);
+    }
     else
+    {
         exit(0);
+    }
+    
 }
 
 int main(int argc, char const *argv[]) {
     char *binaryPath = "/bin/cat";
     catFunction(argv[1]);
+
     return 0;
 }
